0a2277597cdbfd659a3f122f76a0da6c2e595016
Andrei Golubkov <andreygolubkow@gmail.com>
2025-05-06 19:27:49 +0400

    Support search, support attachments

--- TaktTusur.InMemoria.Api/Controllers/PersonController.cs
+++ TaktTusur.InMemoria.Api/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TaktTusur.InMemoria.DataAccess.Context;
+using TaktTusur.InMemoria.Domain.Entities;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -13,16 +15,31 @@ public class PersonController : Controller
 	}
 
 	[HttpGet]
-	public IActionResult Get()
+	public IActionResult Get(string query = "", int skip = 0, int take = 10)
 	{
-		var persons = _context.Persons.ToList();
+		IQueryable<Person> persons = _context.Persons.AsNoTracking().Where(x => x.Active);
+		if (!string.IsNullOrWhiteSpace(query))
+		{
+			persons = persons.Where(x =>
+				x.FirstName.Contains(query)
+				|| x.LastName.Contains(query)
+				|| x.Patronymic.Contains(query)
+				|| x.MaidenName.Contains(query)
+				|| x.Nickname.Contains(query));
+		}
+
+		persons = persons.Skip(skip).Take(take).OrderBy(x => x.LastName).ThenBy(x => x.FirstName).ThenBy(x => x.Patronymic);
+
 		return Ok(persons);
 	}
 
 	[HttpGet("{id}")]
 	public IActionResult Get(int id)
 	{
-		var person = _context.Persons.Find(id);
+		var person = _context.Persons
+			.Include(x => x.Attachments)
+			.AsNoTracking()
+			.FirstOrDefault(x => x.Id == id);
 
 		if (person == null)
 		{
